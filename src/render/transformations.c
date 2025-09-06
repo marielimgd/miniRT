dffa5980803b7f67b8fb2c7305bb7835df66dfe1
Marieli M G Dias <mmariano@student.42sp.org.br>
2025-09-05 21:20:11 -0300

    WIP: transformation functions

--- src/render/transformations.c
+++ src/render/transformations.c
@@ -6,7 +6,7 @@
 /*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 19:55:10 by mmariano          #+#    #+#             */
-/*   Updated: 2025/09/05 20:59:38 by mmariano         ###   ########.fr       */
+/*   Updated: 2025/09/05 21:18:56 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ t_ray	transform(t_ray ray, t_matrix *m)
 {
 	t_ray	new_ray;
 
-	new_ray.origin = tuple_to_vector(*(multiply_matrix_by_tuple(m, ray.origin)));
-	new_ray.direction = tuple_to_vector(multiply_matrix_by_tuple(m, ray.direction));
+	new_ray.origin = tuple_to_vector(multiply_matrix_by_tuple(m, ray.origin));
+	new_ray.direction = tuple_to_vector(*multiply_matrix_by_tuple(m, ray.direction));
 	return (new_ray);
 }
+
 
 void	set_transform(t_object *s, t_matrix *t)
 {
